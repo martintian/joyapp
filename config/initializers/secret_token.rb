77ce8d89aeeb5cde7d22a976a3343bd215d366c3
Martin Tian <martin.tian@gmail.com>
2013-12-17 16:36:13 -0500

    second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Joyapp::Application.config.secret_key_base = '7a69fadfca726e84936855f220e0bf0e4b567d052052ed01e0bd13ebd75f46d609ed04db165c1b1a1c015ce5fbd6f2af7f082a68ac890dbd63044df1878c1c7f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Joyapp::Application.config.session_store :cookie_store, key: '_joyapp_session'
